975fd5d0fd96efd1b9e6bec13a72980894b50203
HermesBlacqk <omotosolilhenxy@gmail.com>
2023-04-26 15:09:09 +0100

    ..........

--- ride-on/src/components/NavBar.vue
+++ ride-on/src/components/NavBar.vue
@@ -3,13 +3,34 @@
     <v-toolbar
       dark
       prominent
-     class="bg-blue"
+     class="bg-black"
     >
+    <!-- <v-toolbar-title>TAXIGEO</v-toolbar-title> -->
+     <v-toolbar-l>
+          <v-img
+  :width="100" 
+  aspect-ratio="16/9"
+  cover
+  src="../assets/logo.jpg"
+></v-img>
+     </v-toolbar-l>
+     <v-spacer></v-spacer>
+   <v-toolbar-search class=""> <label class="bg-white" style="padding: 6px 0;">  <v-icon>mdi-map-marker</v-icon></label>
+          <input  type="text" placeholder="  Tell us your area" class="bg-white"><label><v-btn style="background-color: orange; border-radius: 0;"><v-icon >mdi-arrow-right</v-icon></v-btn></label>
+   </v-toolbar-search>
+     <v-spacer></v-spacer>
       
+<v-nav class=""><ul class="d-flex ">
+         <v-btn class="bg-orange text-white p-4 "> <v-icon icon="mdi-home" />Home</v-btn>
+         <v-btn class=" text-white p-4 "><v-icon class="text-white">mdi-information</v-icon> About</v-btn>
+         <v-btn class=" text-white p-4 "><v-icon class="text-white">mdi-car-cog</v-icon>Services</v-btn>
+         <v-btn class=" text-white p-4 "><v-icon class="text-white">mdi-car-connected</v-icon>Our Rides</v-btn>
+         <v-btn class=" text-white p-4 "><v-icon class="text-white">mdi-phone</v-icon>Contact</v-btn>
+                  
+</ul></v-nav>
+    
 
-      <v-toolbar-title> <v-img></v-img></v-toolbar-title>
-
-      <v-spacer></v-spacer>
+<v-spacer></v-spacer>
 
       <v-btn icon>
         <v-icon>mdi-export</v-icon>
@@ -26,5 +47,49 @@ name: 'NavBar'
 </script>
 
 <style>
+
+ img{
+          width: 100%;
+}
+ img:hover{
+          width: 120%;
+          transition-duration: 0.8s;
+          
+}
+ul{
+          display: flex;
+          justify-content: space-between;
+}
+ul li{
+          list-style: none;
+          padding: 15px 5px;
+          cursor: pointer;
+          border: none;
+          margin:0 10px;
+}
+li:hover{
+          padding: 15px;
+          transition-duration: 0.9s;
+          border-radius: 5px;
+          background: orange;
+          color: white;
+}
+li a{
+          text-decoration: none;
+          
+}
+label{
+          display: inline-block;
+          align-items: center;
+}
+input{
+          padding:6px;
+          border: 1px solid #4f545c;
+          background: rgb(202, 201, 201);
+          outline:none;
+}
+
+
+
 
 </style>
