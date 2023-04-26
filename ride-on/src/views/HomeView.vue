d4dbd3cc6cee1b4c84b1e793c71a209534a0bb38
HermesBlacqk <omotosolilhenxy@gmail.com>
2023-04-26 09:16:06 +0100

    ....

---
+++ ride-on/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="home ">
+  <h1 class="text-h2">home page</h1>
+  <p class="font-weight-black bg-red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium incidunt nemo quia consequuntur recusandae vero voluptate ipsum voluptatem libero quisquam facilis deserunt sunt, beatae possimus iusto blanditiis laudantium praesentium nesciunt.</p>
+</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+
+export default defineComponent({
+  name: 'HomeView',
+
+ 
+});
+</script>
